e7480edd3911bfa72fed703ac3f3337174f15eb7
Michael Ramy <michaelramy97@gmail.com>
2019-04-16 20:57:03 +0200

    Michael's update

---
+++ app/src/main/java/com/example/narim/nova/Search.java
@@ -0,0 +1,42 @@
+package com.example.narim.nova;
+
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+public class Search extends AppCompatActivity {
+
+    TabLayout SearchTabLayout;
+    ViewPager SearchViewPager;
+    TextView SearchBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        SearchTabLayout = findViewById(R.id.TabLayout_Search_Tab);
+        SearchViewPager = findViewById(R.id.ViewPager_Notification);
+        SearchBack=findViewById(R.id.TextView_Search_back);
+        SearchPageTabsAdapter adapter = new SearchPageTabsAdapter(getSupportFragmentManager());
+        adapter.AddFragment("one", new ProfileSearchFragment());
+        adapter.AddFragment("two", new TweetsSearchFragment());
+        SearchTabLayout.setupWithViewPager(SearchViewPager);
+        SearchViewPager.setAdapter(adapter);
+        SearchTabLayout.getTabAt(0).setCustomView(R.layout.profiles_tab_search);
+        SearchTabLayout.getTabAt(1).setCustomView(R.layout.novas_tab_search);
+        SearchBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(Search.this,HomePage.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
